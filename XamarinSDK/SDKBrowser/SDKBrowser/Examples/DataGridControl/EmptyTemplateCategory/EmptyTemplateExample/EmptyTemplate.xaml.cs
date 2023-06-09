c20fe850295130dc5c9cd8fd0e12e28f48b1a738
Polya Pelova <pelova@progress.com>
2023-06-09 12:51:20 +0300

    New version of Progress Telerik UI for Xamarin SDK examples.

---
+++ XamarinSDK/SDKBrowser/SDKBrowser/Examples/DataGridControl/EmptyTemplateCategory/EmptyTemplateExample/EmptyTemplate.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace SDKBrowser.Examples.DataGridControl.EmptyTemplateCategory.EmptyTemplateExample
+{
+    public partial class EmptyTemplate : ContentView
+    {
+        public EmptyTemplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
